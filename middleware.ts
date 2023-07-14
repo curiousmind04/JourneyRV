09903ab6b4a936339509176895429dde31b18029
Brandon Bhangari <b6_10_94_furlonge@hotmail.com>
2023-07-14 15:36:08 -0400

    fixed middleware

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,5 @@
 export { default } from "next-auth/middleware";
 
 export const config = {
-  matcher: ["/trips", "/reservations", "/properties", "/favorites"],
+  matcher: ["/trips", "/reservations", "/vehicles", "/favorites"],
 };
